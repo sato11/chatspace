cfdb63ffbf29c6d4b7250ba1f863a9b272f7f167
Sato Junichi <villanova911@gmail.com>
2016-11-01 00:50:53 +0900

    added not null constraint to groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031151136) do
+ActiveRecord::Schema.define(version: 20161031154824) do
 
   create_table "group_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20161031151136) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
