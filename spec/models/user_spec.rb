bb0d3ea6f840e0f7141f6ad78855c67f78139fe9
Sato Junichi <villanova911@gmail.com>
2016-11-27 10:40:54 +0900

    test if user validation works, i.e. it is invalid with a name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "is valid with name, email, password and password_confirmation" do
+    it "is invalid without name" do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください。")
     end
   end
 end
