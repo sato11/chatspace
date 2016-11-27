a3a3573797798863eba99032ad3064c7fe2e2f28
Sato Junichi <villanova911@gmail.com>
2016-11-27 10:44:08 +0900

    test if it is invalid without an email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,11 @@ describe User do
       user = build(:user)
       expect(user).to be_valid
     end
+
+    it "is invalid without an email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください。")
+    end
   end
 end
