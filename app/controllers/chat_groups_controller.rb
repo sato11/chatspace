775e05cf2d7a8feab93e860d1008db4166a1bde0
Sato Junichi <villanova911@gmail.com>
2016-11-27 23:11:23 +0900

    describe actions and add strong parameters

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -3,14 +3,22 @@ class ChatGroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(create_params)
+    redirect_to :root and return
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
