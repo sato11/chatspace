39b06098bed863cea04b3f2dda7f088e263ba875
Sato Junichi <villanova911@gmail.com>
2016-11-29 13:08:50 +0900

    remove routing and actions what is not relevant in this branch

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -11,12 +11,6 @@ class ChatGroupsController < ApplicationController
     redirect_to :root and return
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   private
   def create_params
     params.require(:group).permit(:name, user_ids: [])
