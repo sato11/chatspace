30cba3d45268f38208a3655d356c04ff1019578e
Sato Junichi <villanova911@gmail.com>
2016-12-02 17:23:30 +0900

    add routing, generate controller, and create view for messages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :chat_groups, except: [:show, :destroy]
+    resources :messages, only: :index
   root to: 'chat_groups#index'
 end
