5fcf4276a2807bd8fa1e47d366a22683fab1cc94
Sato Junichi <villanova911@gmail.com>
2016-12-02 18:06:24 +0900

    make header show current group name and its members

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:chat_group_id])
     @groups = current_user.groups
+    @members = @group.group_members
   end
 end
