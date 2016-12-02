f769052cd7b4e7f12667a48e14972082acec0bb6
Sato Junichi <villanova911@gmail.com>
2016-12-02 17:26:03 +0900

    fix routing description so that :chat_groups nests :messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chat_groups, except: [:show, :destroy]
+  resources :chat_groups, except: [:show, :destroy] do
     resources :messages, only: :index
+  end
   root to: 'chat_groups#index'
 end
