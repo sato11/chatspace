08372d526b56b77408f41b6bc24fee8573741526
Sato Junichi <villanova911@gmail.com>
2016-12-03 19:10:37 +0900

    define private method to set current group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @group = Group.find(params[:chat_group_id])
     @groups = current_user.groups
     @members = @group.group_members
     @message = Message.new
@@ -8,12 +9,15 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @group = Group.find(params[:chat_group_id])
     Message.create(body: create_params[:body], user_id: current_user.id, group_id: @group.id)
     redirect_to chat_group_messages_path and return
   end
 
   private
+  def set_group
+    @group = Group.find(params[:chat_group_id])
+  end
+
   def create_params
     params.require(:message).permit(:body)
   end
