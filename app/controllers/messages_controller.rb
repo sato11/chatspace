d61f172e349991085f9cca053f820d09962010ab
Sato Junichi <villanova911@gmail.com>
2016-12-03 18:24:43 +0900

    add routing and action for messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,7 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @members = @group.group_members
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :chat_groups, except: [:show, :destroy] do
-    resources :messages, only: :index
+    resources :messages, only: [:index, :create]
   end
   root to: 'chat_groups#index'
 end
