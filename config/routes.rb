84850a3d177fc1d3ba6ce3813adb89d537e77ce0
Sato Junichi <villanova911@gmail.com>
2016-12-04 09:00:40 +0900

    rename resources and the related controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chat_groups, except: [:show, :destroy] do
+  resources :groups, except: [:show, :destroy] do
     resources :messages, only: [:index, :create]
   end
-  root to: 'chat_groups#index'
+  root to: 'groups#index'
 end
