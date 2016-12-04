6484ed42dd124fbb3ec385beb8a6faf97af87075
Sato Junichi <villanova911@gmail.com>
2016-12-04 21:40:51 +0900

    write js script using template (this causes error)

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,30 @@
 $(function() {
+  function buildHTML(message) {
+    var html = $('<li class="message">').append(message.content);
+    return html;
+  }
+
   $('#form form').on('submit', function(e) {
     e.preventDefault();
-    message = $('.footer__message').val();
-    console.log(message);
-  })
-})
+    var textField = $('.footer__message');
+    var message = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: '/messages.json',
+      data: {
+        message: {
+          content: message
+        }
+      },
+      datatype: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      textField.val();
+    })
+    .fail(function() {
+      alert('error');
+    })
+  });
+});
