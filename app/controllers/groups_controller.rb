f40a18a292bfdaa56acca71753d4f4baf52a94e2
Sato Junichi <villanova911@gmail.com>
2016-12-15 15:07:45 +0900

    create api using respond_to in the groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,9 +11,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(create_params)
-    if @group.name.present? && @group.group_members.any?
-      @group.save
-      redirect_to :root, notice: 'グループが作成されました' and return
+    if @group.save
+      respond_to do |format|
+        format.html { redirect_to :root, notice: 'グループが作成されました' }
+        format.json { render json: @group }
+      end
     else
       redirect_to new_chat_group_path, alert: 'グループが作成されませんでした' and return
     end
@@ -24,12 +26,11 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if create_params[:name].present?
-      if create_params[:user_ids] == [""]
-        params.require(:group)[:user_ids] << current_user.id.to_s
+    if @group.update
+      respond_to do |format|
+        format.html { redirect_to :root, notice: 'グループが更新されました' }
+        format.json { render json: @group }
       end
-      @group.update(create_params)
-      redirect_to :root, notice: 'グループが更新されました' and return
     else
       redirect_to edit_chat_group_path, alert: 'グループが更新されませんでした' and return
     end
