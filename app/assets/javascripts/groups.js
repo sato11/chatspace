0624da7b6e18443d45ae618a7399d8983bd126c7
Sato Junichi <villanova911@gmail.com>
2016-12-16 16:04:36 +0900

    append user name when input value matches the name

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,11 +1,24 @@
 $(function() {
+  function getUser(users, input) {
+    $.each(users, function(i, user) {
+      var name = user.name;
+      if (input == name) {
+        var matchedUser = name;
+        var list = $('<li>').append(matchedUser + '</li>')
+        $('#user-search-result').append(list)
+      }
+    });
+  }
+
   $('#search').on('click', function(e) {
     e.preventDefault();
+    $('#user-search-result li').remove();
     var textField = $('#user-search-field .chat-group-form__input');
-    var input = textField.val;
+    var input = textField.val();
     $.ajax('/groups/new.json')
       .done(function(data) {
-        console.log(data);
+        getUser(data, input);
+        textField.val('');
       })
       .fail(function() {
         alert('error');
