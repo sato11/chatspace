985822690fcc819f5b903009acf2369867e8a8ce
Sato Junichi <villanova911@gmail.com>
2016-12-17 14:24:23 +0900

    configure js to show user when input value matches his name

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,22 +1,34 @@
 $(function() {
+  function buildHTML(name) {
+    var html = $('<li class="result-list">').append(
+                    '<span class="result-list--left">' +
+                      name +
+                    '</span>' +
+                    '<span class="result-list--right">' +
+                      "追加" +
+                    '</span>' +
+                  '</li>');
+    $('#result-field').append(html);
+    return html;
+  }
+
   function getUser(users, input) {
     $.each(users, function(i, user) {
       var name = user.name;
       if (input == name) {
-        var matchedUser = name;
-        var list = $('<li>').append(matchedUser + '</li>')
-        $('#result-field').append(list)
+        var html = buildHTML(name);
+        return false;
       }
     });
   }
 
   $('#search').on('click', function(e) {
     e.preventDefault();
-    $('#user-search-result li').remove();
     var textField = $('#user-search-field .chat-group-form__input');
     var input = textField.val();
     $.ajax('/groups/new.json')
       .done(function(data) {
+        $('#result-field li').remove();
         getUser(data, input);
         textField.val('');
       })
