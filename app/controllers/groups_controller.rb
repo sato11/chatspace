2057cdfb5c7f4dfd6f502c7f872a4b469db507a7
Sato Junichi <villanova911@gmail.com>
2016-12-17 19:20:27 +0900

    move respond_to from #update to #edit: also roll back #update action to the original description

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,14 +26,21 @@ class GroupsController < ApplicationController
 
   def edit
     @members = @group.group_members
+    users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: users }
+    end
   end
 
   def update
-    if @group.update
-      respond_to do |format|
-        format.html { redirect_to :root, notice: 'グループが更新されました' }
-        format.json { render json: @group }
+    if create_params[:name].present?
+      if create_params[:user_ids] == [""]
+        params.require(:group)[:user_ids] << current_user.id.to_s
       end
+      @group.update(create_params)
+      redirect_to :root, notice: 'グループが更新されました' and return
+
     else
       redirect_to edit_group_path, alert: 'グループが更新されませんでした' and return
     end
