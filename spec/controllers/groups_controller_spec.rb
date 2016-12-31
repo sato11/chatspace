7aee3543bd99cc5dd6f7f58b13b5132f5c34023f
Sato Junichi <villanova911@gmail.com>
2016-12-31 14:57:52 +0900

    configure to let mock user sign in to pass devise authentication

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe GroupsController do
   describe "GET index" do
+    login_user
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/controller_macros'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -58,4 +59,5 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, :type => :controller
 end
