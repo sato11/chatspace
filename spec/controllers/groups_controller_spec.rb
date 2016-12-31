c9844614a28461339f0f2548874b2514e1f46c58
Sato Junichi <villanova911@gmail.com>
2016-12-31 19:49:16 +0900

    test if @groups in groups#index is array

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe GroupsController do
-  describe "GET index" do
+  describe 'GET #index' do
     login_user
-    it "renders the :index template" do
+    it "has @groups as an array" do
+      get :index
+      expect(assigns(:groups)).to match_array([])
+    end
+
+    it 'renders the :index template' do
       get :index
       expect(response).to render_template :index
     end
