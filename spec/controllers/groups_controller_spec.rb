73f4cb37b5bab2c6553a2c927c0fc06417417cfc
Sato Junichi <villanova911@gmail.com>
2016-12-31 19:55:51 +0900

    test if groups#new renders proper template

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -13,4 +13,12 @@ describe GroupsController do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #new' do
+    login_user
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
