f338726bf4cdd99139d26974ef6a1bc4b21e5f49
Sato Junichi <villanova911@gmail.com>
2017-01-03 18:08:47 +0900

    unify shared action into a subject clause to organize codes

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -16,19 +16,20 @@ describe Group do
 
   describe 'check_member' do
     let(:user) { create(:user) }
+    subject { group.check_member }
     context 'when the group has any member' do
       let(:group) { create(:group, { user_ids: @group_attrs[:user_ids] }) }
       it 'returns true' do
         @group_attrs = attributes_for(:group, user_ids: [])
         @group_attrs[:user_ids] << user.id.to_s
-        expect(group.check_member).to eq true
+        is_expected.to eq true
       end
     end
 
     context 'when the group has no member' do
       let(:group) { create(:group) }
       it 'returns false' do
-        expect(group.check_member).to eq false
+        is_expected.to eq false
       end
     end
   end
