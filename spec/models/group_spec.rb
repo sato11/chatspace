55bf4fd5dd72f06f4d2eb222b95bf1238b0e673d
Sato Junichi <villanova911@gmail.com>
2017-01-03 17:35:25 +0900

    test to check group without name is not created

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 describe Group do
   describe '#create' do
+    it 'is invalid without a name' do
+      group = build(:group, name: "")
+      group.valid?
+      expect(group.errors[:name]).to include("を入力してください。")
+    end
   end
 end
