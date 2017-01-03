daa149e4047367b758609771ceafdf06e3edd415
Sato Junichi <villanova911@gmail.com>
2017-01-03 18:02:47 +0900

    test if instance method returns adequate value when the group has a member

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -15,10 +15,21 @@ describe Group do
   end
 
   describe 'check_member' do
+    let(:user) { create(:user) }
     context 'when the group has any member' do
+      let(:group) { create(:group, { user_ids: @group_attrs[:user_ids] }) }
+      it 'returns true' do
+        @group_attrs = attributes_for(:group, user_ids: [])
+        @group_attrs[:user_ids] << user.id.to_s
+        expect(group.check_member).to eq true
+      end
     end
 
     context 'when the group has no member' do
+      let(:group) { create(:group) }
+      it 'returns false' do
+        expect(group.check_member).to eq false
+      end
     end
   end
 end
