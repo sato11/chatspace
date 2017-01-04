767e78e3d89a778cd73a560de38f3422197061ba
Sato Junichi <villanova911@gmail.com>
2017-01-04 22:27:31 +0900

    add sample image under spec/support and let factory girl use it to generate mock object

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :message do
     body  Faker::Lorem.sentence
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'messages', 'images', 'test_image.jpeg')) }
     user  { create(:user) }
     group { create(:group) }
   end
