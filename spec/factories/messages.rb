52b437ce8bee92672f94c48fb43f3b49a9fdf0e4
Sato Junichi <villanova911@gmail.com>
2017-01-04 23:20:40 +0900

    modify path to the sample image

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     body  Faker::Lorem.sentence
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'messages', 'images', 'test_image.jpeg')) }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'messages', 'images', 'test_image.jpeg')) }
     user  { build(:user) }
     group { build(:group) }
   end
