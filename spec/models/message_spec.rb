e96ff843224c08145e62ba5da972098508ce11e8
Sato Junichi <villanova911@gmail.com>
2017-01-04 22:40:31 +0900

    test if it is valid with body and image

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
+    it 'is valid with body and image' do
+      message = build(:message)
+      expect(message).to be_valid
+    end
   end
 end
