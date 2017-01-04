aa925976599a207f456be1672bdce1c4121ba88b
Sato Junichi <villanova911@gmail.com>
2017-01-04 22:54:09 +0900

    test if mount_uploader attaches an image to the message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 describe Message do
+  describe 'mount_uploader' do
+    it 'attaches an image to the message' do
+      message = build(:message)
+      expect(message.image).not_to be_nil
+    end
+  end
+
   describe '#create' do
     it 'is valid with body and image' do
       message = build(:message)
