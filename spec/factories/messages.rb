da837efa8278e43cd33a95d7fb1f56940cb2850d
Sato Junichi <villanova911@gmail.com>
2017-01-04 22:31:03 +0900

    use build instead of create in factory girl

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :message do
     body  Faker::Lorem.sentence
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'messages', 'images', 'test_image.jpeg')) }
-    user  { create(:user) }
-    group { create(:group) }
+    user  { build(:user) }
+    group { build(:group) }
   end
 end
