45d21135083a84e9e40d2da2db11d6537f5fa3a9
Sato Junichi <villanova911@gmail.com>
2017-01-05 11:29:40 +0900

    add context and description of what is going to be tested

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe MessagesController do
+  context 'when user is signed in' do
+    describe 'GET #index' do
+    end
+
+    describe 'POST #create' do
+    end
+  end
+
+  context 'when user is not signed in' do
+    describe 'GET #index' do
+    end
+
+    describe 'POST #create' do
+    end
+  end
+end
