d730813bc87175b2bdc90034651ac094fac21f21
Sato Junichi <villanova911@gmail.com>
2017-01-05 11:38:31 +0900

    test if messages#index has groups as an array

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe MessagesController do
   context 'when user is signed in' do
+    login_user
+    let(:group) { create(:group) }
+
     describe 'GET #index' do
+      it 'has @groups as an array' do
+        get :index, params: { group_id: group }
+        expect(assigns(:groups)).to match_array []
+      end
     end
 
     describe 'POST #create' do
