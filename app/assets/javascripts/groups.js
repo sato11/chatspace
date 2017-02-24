9705acc92d11bc0b080fdcb6e9d45378ed0bdc6b
Sato Junichi <villanova911@gmail.com>
2017-02-24 20:05:17 +0900

    modify indentation: require 4 spaces

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,70 +1,70 @@
 $( function() {
-  var $textField = $( '#user-search-field input' );
+    var $textField = $( '#user-search-field input' );
 
-  // 検索結果のユーザーを表示するためのHTMLを作成する
-  function addUserHTML( user ) {
-    var html = $( `
-      <li class="result-list">
-        <span class="result-list--left">${user.name}</span>
-        <span class="result-list--right">追加</span>
-        <input value="${user.id}" type="hidden">
-      </li>
-      ` );
-    $( '#result-field' ).append( html );
-  }
+    // 検索結果のユーザーを表示するためのHTMLを作成する
+    function addUserHTML( user ) {
+        var html = $( `
+            <li class="result-list">
+                <span class="result-list--left">${user.name}</span>
+                <span class="result-list--right">追加</span>
+                <input value="${user.id}" type="hidden">
+            </li>
+          ` );
+        $( '#result-field' ).append( html );
+    }
 
-  // jsonで受け取ったUsers.allの配列を入力された値と照合し、一致するユーザーを判定する
-  function getUser( users, input ) {
-    var reg = RegExp( '^' + input );
-    $.each( users, function( i, user ) {
-      if ( input.length !== 0 && user.name.match( reg )) { addUserHTML( user ); }
+    // jsonで受け取ったUsers.allの配列を入力された値と照合し、一致するユーザーを判定する
+    function getUser( users, input ) {
+        var reg = RegExp( '^' + input );
+        $.each( users, function( i, user ) {
+            if ( input.length !== 0 && user.name.match( reg )) { addUserHTML( user ); }
+        });
+    }
+
+      // ユーザー検索時にjsonと通信する
+    $textField.on( 'keyup', function( e ) {
+        e.preventDefault();
+        var input = $textField.val();
+        $.ajax( '/groups/new.json' )
+            .done( function( data ) {
+                $( '#result-field li' ).remove();
+                getUser( data, input );
+            })
+            .fail( function() {
+                alert( 'error' );
+            });
     });
-  }
 
-  // ユーザー検索時にjsonと通信する
-  $textField.on( 'keyup', function( e ) {
-    e.preventDefault();
-    var input = $textField.val();
-    $.ajax( '/groups/new.json' )
-      .done( function( data ) {
-        $( '#result-field li' ).remove();
-        getUser( data, input );
-      })
-      .fail( function() {
-        alert( 'error' );
-      });
-  });
+    // ユーザー追加時にチャットメンバーに表示するためのHTMLを作成する
+    function addedUserHTML( name, id ) {
+        var $html = $( `
+            <li class="chat-group-user">
+                <div class="chat-group-user__name">${name}</div>
+                <div class="chat-group-user__btn chat-group-user__btn--remove">削除</div>
+                <input value="${id}" type="hidden">
+            </li>
+          ` );
+        return $html;
+    }
 
-  // ユーザー追加時にチャットメンバーに表示するためのHTMLを作成する
-  function addedUserHTML( name, id ) {
-    var $html = $( `
-      <li class="chat-group-user">
-        <div class="chat-group-user__name">${name}</div>
-        <div class="chat-group-user__btn chat-group-user__btn--remove">削除</div>
-        <input value="${id}" type="hidden">
-      </li>
-      ` );
-    return $html;
-  }
+    // 追加ボタンがクリックされた時に該当するユーザーの名前とidを取得してチャットメンバーに表示する
+    $( document ).on( 'click', '.result-list--right', function() {
+        var name = $( this ).prev().html();
+        var id = $( this ).next().attr( 'value' );
+        var html = addedUserHTML( name, id );
+        $( '#added-users' ).append( html );
+        $( '#user-search-field .chat-group-form__input' ).val( '' );
+        // collection_check_boxesの該当ユーザーにチェックを入れる
+        $( `input[value=${id}]` ).prop( 'checked', true );
+        // 検索結果を初期化する
+        $( '.result-list' ).remove();
+    });
 
-  // 追加ボタンがクリックされた時に該当するユーザーの名前とidを取得してチャットメンバーに表示する
-  $( document ).on( 'click', '.result-list--right', function() {
-    var name = $( this ).prev().html();
-    var id = $( this ).next().attr( 'value' );
-    var html = addedUserHTML( name, id );
-    $( '#added-users' ).append( html );
-    $( '#user-search-field .chat-group-form__input' ).val( '' );
-    // collection_check_boxesの該当ユーザーにチェックを入れる
-    $( `input[value=${id}]` ).prop( 'checked', true );
-    // 検索結果を初期化する
-    $( '.result-list' ).remove();
-  });
-
-  // 削除ボタンがクリックされた時に該当するユーザーをチャットメンバーから削除する
-  $( document ).on( 'click', '.chat-group-user__btn--remove', function() {
-    $( this ).parent().remove();
-    // collection_check_boxesの該当ユーザーからチェックを外す
-    var id = $( this ).next().attr( 'value' );
-    $( `input[value=${id}]` ).prop( 'checked', false );
-  });
+    // 削除ボタンがクリックされた時に該当するユーザーをチャットメンバーから削除する
+    $( document ).on( 'click', '.chat-group-user__btn--remove', function() {
+        $( this ).parent().remove();
+        // collection_check_boxesの該当ユーザーからチェックを外す
+        var id = $( this ).next().attr( 'value' );
+        $( `input[value=${id}]` ).prop( 'checked', false );
+    });
 });
--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,99 +1,99 @@
 $( function() {
-  // 変数の定義
-  var $messageForm = $( '#new_message' );
-  var $fileInput = $( '#file-input' );
-  var $textField = $( '#message_body' );
-  var imageChecker = $( 'input[type=file]' )[0].files.length;
+    // 変数の定義
+    var $messageForm = $( '#new_message' );
+    var $fileInput = $( '#file-input' );
+    var $textField = $( '#message_body' );
+    var imageChecker = $( 'input[type=file]' )[0].files.length;
 
-  scrollToBottom();
+    scrollToBottom();
 
-  // チャット画面の最下部まで移動する関数
-  function scrollToBottom() {
-    var $messageBoard = $( '.message-board__content' );
-    setTimeout( function(){
-      $messageBoard.animate({
-        scrollTop: $messageBoard.prop( 'scrollHeight' )
-      }, 1000 );
-    }, 100 );
-  }
-
-  // チャット画面において30秒おきに画面を更新する
-  if ( $( 'body' ).attr( 'controller' ) == 'messages' ) {
-    setInterval( function() {
-      window.location.reload();
-    }, 30000 );
-  }
-
-  // メッセージ毎にhtmlを構築する作業を関数として定義
-  // 部分テンプレート_messageの構造に沿う
-  function buildHTML( message ) {
-    var user = $( '.sidebar__top-left' ).html();
-    // 画像が存在しないときは空にすることでエラーを避ける
-    if ( imageChecker === 0 ) { message.image = ''; }
-    var $html = $( `
-      <li class="message">
-        <div class="message__user">
-          <span class="message__user-name">${user}</span>
-          <span class="message__posted-time">${message.time}</span>
-        <div class="message__content">${message.body}
-          <div class="message__image">
-            <img src="${message.image}">
-          </div>
-        </div>
-      </li>
-    ` );
-    return $html;
-  }
-
-  // formDataの形式を整える
-  function buildFormData( f ) {
-    var fd = new FormData( f.get( 0 ));
-    var message = $( '.footer__message' ).val();
-    var file = $( 'input[type=file]' )[0].files[0];
-    fd.append( 'body', message );
-    fd.append( 'image', file );
-
-    return fd;
-  }
-
-  // 引数で渡されたフォームの選択を解除する
-  function resetInput( e ) {
-    e.wrap( '<form>' ).closest( 'form' ).get( 0 ).reset();
-    e.unwrap();
-  }
-
-  // 画像は選択されると自動的に送信される
-  $fileInput.on( 'change', function() {
-    imageChecker = 1; // 一時的に変数を書き換えてバリデーションを通す
-    $messageForm.trigger( 'submit' );
-    resetInput( $( this ));
-  });
-
-  // submit時にjsonで非同期通信を行う
-  $messageForm.on( 'submit', function( e ) {
-    e.preventDefault();
-    // テキストと画像の両方が空の際は処理に進まないバリデーション
-    if ( !$textField.val() && imageChecker === 0 ) {
-      alert( 'either text or image is required' );
-    } else {
-      var formData = buildFormData( $( this ));
-      $.ajax({
-        type: 'POST',
-        url: './messages.json',
-        data: formData,
-        processData: false,
-        contentType: false
-      })
-      .done( function( data ) {
-        $( '.messages' ).append( buildHTML( data ));
-        resetInput( $( '#message_body' ));
-        imageChecker = $( 'input[type=file]' )[0].files.length; // 変数を初期化する
-        scrollToBottom();
-      })
-      .fail( function() {
-        alert( 'error communicating' );
-      });
-      return false;
+    // チャット画面の最下部まで移動する関数
+    function scrollToBottom() {
+        var $messageBoard = $( '.message-board__content' );
+        setTimeout( function(){
+            $messageBoard.animate({
+                scrollTop: $messageBoard.prop( 'scrollHeight' )
+            }, 1000 );
+        }, 100 );
     }
-  });
+
+    // チャット画面において30秒おきに画面を更新する
+    if ( $( 'body' ).attr( 'controller' ) == 'messages' ) {
+        setInterval( function() {
+            window.location.reload();
+        }, 30000 );
+    }
+
+    // メッセージ毎にhtmlを構築する作業を関数として定義
+    // 部分テンプレート_messageの構造に沿う
+    function buildHTML( message ) {
+        var user = $( '.sidebar__top-left' ).html();
+        // 画像が存在しないときは空にすることでエラーを避ける
+        if ( imageChecker === 0 ) { message.image = ''; }
+        var $html = $( `
+            <li class="message">
+                <div class="message__user">
+                    <span class="message__user-name">${user}</span>
+                    <span class="message__posted-time">${message.time}</span>
+                <div class="message__content">${message.body}
+                    <div class="message__image">
+                        <img src="${message.image}">
+                    </div>
+                </div>
+            </li>
+        ` );
+        return $html;
+    }
+
+    // formDataの形式を整える
+    function buildFormData( f ) {
+        var fd = new FormData( f.get( 0 ));
+        var message = $( '.footer__message' ).val();
+        var file = $( 'input[type=file]' )[0].files[0];
+        fd.append( 'body', message );
+        fd.append( 'image', file );
+
+        return fd;
+    }
+
+    // 引数で渡されたフォームの選択を解除する
+    function resetInput( e ) {
+        e.wrap( '<form>' ).closest( 'form' ).get( 0 ).reset();
+        e.unwrap();
+    }
+
+    // 画像は選択されると自動的に送信される
+    $fileInput.on( 'change', function() {
+        imageChecker = 1; // 一時的に変数を書き換えてバリデーションを通す
+        $messageForm.trigger( 'submit' );
+        resetInput( $( this ));
+    });
+
+    // submit時にjsonで非同期通信を行う
+    $messageForm.on( 'submit', function( e ) {
+        e.preventDefault();
+        // テキストと画像の両方が空の際は処理に進まないバリデーション
+        if ( !$textField.val() && imageChecker === 0 ) {
+            alert( 'either text or image is required' );
+        } else {
+            var formData = buildFormData( $( this ));
+            $.ajax({
+                type: 'POST',
+                url: './messages.json',
+                data: formData,
+                processData: false,
+                contentType: false
+            })
+            .done( function( data ) {
+                $( '.messages' ).append( buildHTML( data ));
+                resetInput( $( '#message_body' ));
+                imageChecker = $( 'input[type=file]' )[0].files.length; // 変数を初期化する
+                scrollToBottom();
+            })
+            .fail( function() {
+                alert( 'error communicating' );
+            });
+            return false;
+        }
+    });
 });
