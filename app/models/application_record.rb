dc53c6372ed5b8c7f2a3cfac4f1fd91363af92f8
Sato Junichi <villanova911@gmail.com>
2017-02-28 19:45:15 +0900

    do not show year if it is this year

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,6 +6,8 @@ class ApplicationRecord < ActiveRecord::Base
       created_at.strftime('%T')
     elsif created_at > Time.now.ago(3.days)
       created_at.strftime('%A')
+    elsif created_at > Time.now.beginning_of_year
+      created_at.strftime('%-m/%d')
     else
       created_at.strftime('%F')
     end
