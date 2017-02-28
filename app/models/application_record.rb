4dd0494405e4622a64c87b3f45a1e957bb80662d
Sato Junichi <villanova911@gmail.com>
2017-02-28 20:43:35 +0900

    define variable to act as a receiver

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,14 +2,15 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def sidebar_time
-    if created_at.today?
-      created_at.strftime('%T')
-    elsif created_at > Time.now.ago(3.days)
-      created_at.strftime('%A')
-    elsif created_at > Time.now.beginning_of_year
-      created_at.strftime('%-m/%d')
+    c = created_at
+    if c.today?
+      c.strftime('%T')
+    elsif c > Time.now.ago(3.days)
+      c.strftime('%A')
+    elsif c > Time.now.beginning_of_year
+      c.strftime('%-m/%d')
     else
-      created_at.strftime('%F')
+      c.strftime('%F')
     end
   end
 end
