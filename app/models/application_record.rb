e669ce33636a6ae6ed5df10d06a06671f7097b9b
Sato Junichi <villanova911@gmail.com>
2017-02-28 19:21:04 +0900

    change strftime parameter according to the message's date

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def sidebar_time
+    if created_at.today?
+      created_at.strftime('%T')
+    elsif created_at > Time.now.ago(3.days)
+      created_at.strftime('%A')
+    else
+      created_at.strftime('%F')
+    end
+  end
 end
