81164fb72fa023b2d0d746efa58d094c4506022a
Sato Junichi <villanova911@gmail.com>
2017-03-30 19:23:14 +0900

    test group model's methods

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,9 +3,9 @@ describe Group do
   describe '#create' do
     context 'when a group does not have a name' do
       it 'is invalid' do
-        group = build(:group, name: "")
+        group = build(:group, name: '')
         group.valid?
-        expect(group.errors[:name]).to include("を入力してください。")
+        expect(group.errors[:name]).to include('を入力してください。')
       end
     end
 
@@ -18,11 +18,12 @@ describe Group do
     end
   end
 
-  describe 'check_member' do
+  describe '#check_member' do
     let(:user) { create(:user) }
     subject { group.check_member }
+
     context 'when the group has any member' do
-      let(:group) { create(:group, { user_ids: @group_attrs[:user_ids] }) }
+      let(:group) { create(:group, user_ids: @group_attrs[:user_ids]) }
       it 'returns true' do
         @group_attrs = attributes_for(:group, user_ids: [])
         @group_attrs[:user_ids] << user.id.to_s
@@ -34,6 +35,51 @@ describe Group do
       let(:group) { create(:group) }
       it 'returns false' do
         is_expected.to eq false
+      end
+    end
+  end
+
+  describe '#check_content' do
+    let(:group) { create(:group) }
+    subject { group.check_content }
+
+    context 'when the group has a message' do
+      context 'when the message has an image' do
+        it 'announces that the image is sent' do
+          @message = create(:message, group: group)
+          is_expected.to eq '画像が送信されました'
+        end
+      end
+
+      context 'when the message has no image' do
+        it "returns the message's body" do
+          @message = create(:message, image: '', group: group)
+          is_expected.to eq @message.body
+        end
+      end
+    end
+
+    context 'when the group has no message' do
+      it 'points out that the group has no messages yet' do
+        is_expected.to eq 'まだメッセージがありません'
+      end
+    end
+  end
+
+  describe '#check_time' do
+    let(:group) { create(:group) }
+    subject { group.check_time }
+
+    context 'when the group has a message' do
+      it 'returns the time the message is created' do
+        message = create(:message, group: group)
+        is_expected.to eq message.created_at.strftime('%T')
+      end
+    end
+
+    context 'when the group has no message' do
+      it 'returns the time the group is created' do
+        is_expected.to eq group.created_at.strftime('%T')
       end
     end
   end
