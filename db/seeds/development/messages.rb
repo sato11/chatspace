e732c33f16ccc5745e27a14c8f3ae1e9668f34f7
Sato Junichi <villanova911@gmail.com>
2017-04-15 18:46:19 +0900

    sharpen the definition of seed association

--- db/seeds/development/messages.rb
+++ db/seeds/development/messages.rb
@@ -2,12 +2,14 @@ require 'faker'
 I18n.locale = :en
 
 users = User.all
-groups = Group.all
 
 20_000.times do
+  user = users.sample
+  group = user.groups.sample
+
   Message.create!(
     body: Faker::Lorem.sentence(3, true, 30),
-    user: users.sample,
-    group: groups.sample
+    user: user,
+    group: group
   )
 end
