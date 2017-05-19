278b16b2b856fe312792eea837a88293fa8c8303
Sato Junichi <villanova911@gmail.com>
2017-05-19 20:34:14 +0900

    allow to search user by lowercased characters

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -17,7 +17,9 @@ $( function() {
     function getUser( users, input ) {
         var reg = RegExp( '^' + input );
         $.each( users, function( i, user ) {
-            if ( input.length !== 0 && user.name.match( reg )) { addUserHTML( user ); }
+            $.each( user.name.toLowerCase().split( ' ' ), function( i, name ) {
+                if ( input.length !== 0 && name.match( reg )) { addUserHTML( user ); }
+            });
         });
     }
 
