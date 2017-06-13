4265714152d96269bea9338a1bca1c82c61851a6
Sato Junichi <villanova911@gmail.com>
2017-06-13 21:27:26 +0900

    add error handling system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers
+
+  protected
+
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
