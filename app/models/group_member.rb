6d878ba8b4c2b826b03e8b1c46dfa10b5214178b
Sato Junichi <villanova911@gmail.com>
2017-06-19 22:40:15 +0900

    annotate models

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: group_members
+#
+#  id         :integer          not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_group_members_on_group_id  (group_id)
+#  index_group_members_on_user_id   (user_id)
+#
+
 class GroupMember < ApplicationRecord
   belongs_to :user
   belongs_to :group
