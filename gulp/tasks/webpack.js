2172dbb0cc67e9e98a57afc5e5986affa660c378
Sato Junichi <villanova911@gmail.com>
2017-08-15 18:16:53 +0900

    add digest suffix to compiled js file

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -4,9 +4,13 @@ const config        = require( './config' );
 const gulp          = require( 'gulp' );
 const webpack       = require( 'gulp-webpack' );
 const webpackConfig = require( './webpack.config.js' );
+const rev           = require( 'gulp-rev' );
 
 gulp.task( 'webpack', () => {
     gulp.src( config.javascript.src )
         .pipe( webpack( webpackConfig ))
-        .pipe( gulp.dest( webpackConfig.output.publicPath ));
+        .pipe( rev())
+        .pipe( gulp.dest( webpackConfig.output.publicPath ))
+        .pipe( rev.manifest( config.rev.dest, config.rev.opts ))
+        .pipe( gulp.dest( config.publicAssets ));
 });
