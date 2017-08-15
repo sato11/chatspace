752ba0e17d5e3940040538225e0b521f7f25a2b3
Sato Junichi <villanova911@gmail.com>
2017-08-15 16:16:39 +0900

    enable gulp-sass-glob to use wildcard in import syntax

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,11 +1,13 @@
 /*eslint-env node*/
 
-const config = require( './config' );
-const gulp   = require( 'gulp' );
-const sass   = require( 'gulp-sass' );
+const config   = require( './config' );
+const gulp     = require( 'gulp' );
+const sass     = require( 'gulp-sass' );
+const sassGlob = require( 'gulp-sass-glob' );
 
 gulp.task( 'compile-scss', () => {
     gulp.src( config.stylesheet.srcScss )
+        .pipe( sassGlob())
         .pipe( sass())
         .pipe( gulp.dest( config.stylesheet.dest ));
 });
