08ef79c760001ca88fb24358fc17aa1fc3fb37e6
Sato Junichi <villanova911@gmail.com>
2017-08-15 16:30:45 +0900

    minify css

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,16 +1,18 @@
 /*eslint-env node*/
 
-const config   = require( './config' );
-const gulp     = require( 'gulp' );
-const sass     = require( 'gulp-sass' );
-const sassGlob = require( 'gulp-sass-glob' );
-const rename   = require( 'gulp-rename' );
-const rev      = require( 'gulp-rev' );
+const config    = require( './config' );
+const gulp      = require( 'gulp' );
+const sass      = require( 'gulp-sass' );
+const sassGlob  = require( 'gulp-sass-glob' );
+const rename    = require( 'gulp-rename' );
+const rev       = require( 'gulp-rev' );
+const minifyCss = require( 'gulp-minify-css' );
 
 gulp.task( 'compile-scss', () => {
     gulp.src( config.stylesheet.srcScss )
         .pipe( sassGlob())
         .pipe( sass())
+        .pipe( minifyCss())
         .pipe( rename({ suffix: '.bundle' }))
         .pipe( rev())
         .pipe( gulp.dest( config.stylesheet.dest ))
