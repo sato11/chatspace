1fd3c4453cf394b7c4087ec83b7f83cff5a33820
Sato Junichi <villanova911@gmail.com>
2017-08-15 18:18:42 +0900

    uglify application.js

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -5,10 +5,12 @@ const gulp          = require( 'gulp' );
 const webpack       = require( 'gulp-webpack' );
 const webpackConfig = require( './webpack.config.js' );
 const rev           = require( 'gulp-rev' );
+const uglify        = require( 'gulp-uglify' );
 
 gulp.task( 'webpack', () => {
     gulp.src( config.javascript.src )
         .pipe( webpack( webpackConfig ))
+        .pipe( uglify())
         .pipe( rev())
         .pipe( gulp.dest( webpackConfig.output.publicPath ))
         .pipe( rev.manifest( config.rev.dest, config.rev.opts ))
