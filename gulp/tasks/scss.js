2073768aaa0cd6c01c43d76d487cd3c83e348cf4
Sato Junichi <villanova911@gmail.com>
2017-08-15 16:28:22 +0900

    suffix digest and .bundle to compiled css

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,10 +4,16 @@ const config   = require( './config' );
 const gulp     = require( 'gulp' );
 const sass     = require( 'gulp-sass' );
 const sassGlob = require( 'gulp-sass-glob' );
+const rename   = require( 'gulp-rename' );
+const rev      = require( 'gulp-rev' );
 
 gulp.task( 'compile-scss', () => {
     gulp.src( config.stylesheet.srcScss )
         .pipe( sassGlob())
         .pipe( sass())
-        .pipe( gulp.dest( config.stylesheet.dest ));
+        .pipe( rename({ suffix: '.bundle' }))
+        .pipe( rev())
+        .pipe( gulp.dest( config.stylesheet.dest ))
+        .pipe( rev.manifest( config.rev.dest, config.rev.opts ))
+        .pipe( gulp.dest( config.publicAssets ));
 });
