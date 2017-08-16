4d75d0aa506a352ab51b65d389a0fd48a4a8adbf
Sato Junichi <villanova911@gmail.com>
2017-08-16 16:28:42 +0900

    remove gulp-rev and gulp-rename

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,8 +4,6 @@ const config    = require( './config' );
 const gulp      = require( 'gulp' );
 const sass      = require( 'gulp-sass' );
 const sassGlob  = require( 'gulp-sass-glob' );
-const rename    = require( 'gulp-rename' );
-const rev       = require( 'gulp-rev' );
 const minifyCss = require( 'gulp-minify-css' );
 const notify    = require( 'gulp-notify' );
 const gulpIf    = require( 'gulp-if' );
@@ -15,10 +13,6 @@ gulp.task( 'compile-scss', ['installBower'], () => {
         .pipe( sassGlob())
         .pipe( sass())
         .pipe( gulpIf( config.isProduction, minifyCss()))
-        .pipe( gulpIf( config.isProduction, rename({ suffix: '.bundle' })))
-        .pipe( gulpIf( config.isProduction, rev()))
         .pipe( gulp.dest( config.stylesheet.dest ))
-        .pipe( gulpIf( config.isProduction, rev.manifest( config.rev.dest, config.rev.opts )))
-        .pipe( gulpIf( config.isProduction, gulp.dest( config.publicAssets )))
         .pipe( gulpIf( !config.isProduction, notify( 'finish compile-scss' )));
 });
--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -4,7 +4,6 @@ const config        = require( './config' );
 const gulp          = require( 'gulp' );
 const webpack       = require( 'gulp-webpack' );
 const webpackConfig = require( './webpack.config.js' );
-const rev           = require( 'gulp-rev' );
 const uglify        = require( 'gulp-uglify' );
 const notify        = require( 'gulp-notify' );
 const gulpIf        = require( 'gulp-if' );
@@ -13,9 +12,6 @@ gulp.task( 'webpack', () => {
     gulp.src( config.javascript.src )
         .pipe( webpack( webpackConfig ))
         .pipe( gulpIf( config.isProduction, uglify()))
-        .pipe( gulpIf( config.isProduction, rev()))
         .pipe( gulp.dest( webpackConfig.output.publicPath ))
-        .pipe( gulpIf( config.isProduction, rev.manifest( config.rev.dest, config.rev.opts )))
-        .pipe( gulpIf( config.isProduction, gulp.dest( config.publicAssets )))
         .pipe( gulpIf( !config.isProduction, notify( 'finish webpack' )));
 });
